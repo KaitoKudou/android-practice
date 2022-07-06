69d9a97705f8a1ce5a9fca0d16bee36126ede123
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-07-06 18:08:38 +0900

    [add]画面を追加

---
+++ janken/app/src/main/java/com/gmail/kaitokudou2468110921/janken/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.kaitokudou2468110921.janken
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
