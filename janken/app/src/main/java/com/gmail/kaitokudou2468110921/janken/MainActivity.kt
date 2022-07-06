cb58e89f068721221de0ab442e4a93fa0d53d505
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-07-06 19:15:22 +0900

    [add]Intentで遷移先に自分の手の情報を渡す

--- janken/app/src/main/java/com/gmail/kaitokudou2468110921/janken/MainActivity.kt
+++ janken/app/src/main/java/com/gmail/kaitokudou2468110921/janken/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.gmail.kaitokudou2468110921.janken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.gmail.kaitokudou2468110921.janken.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gu.setOnClickListener { onJankenButtonTapped(it) }
+        binding.choki.setOnClickListener { onJankenButtonTapped(it) }
+        binding.pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND", view?.id)
+        startActivity(intent)
     }
 }
