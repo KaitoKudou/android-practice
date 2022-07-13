caecefdf6376f5cf22e050129c25f9a741d5bf8c
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-07-13 16:27:36 +0900

    [add]プロジェクト作成

---
+++ MySize/app/src/main/java/com/gmail/kaitokudou2468110921/mysize/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.kaitokudou2468110921.mysize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
